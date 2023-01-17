8125fb03c4d7400413bdb4e32802bf692ad93fb5
Simin-Kim <show3391@gmail.com>
2023-01-17 17:22:53 +0900

    기본 프로그램 클릭 시 라우팅 구현

--- src/components/program/GoodByeProgramComponent.tsx
+++ src/components/program/GoodByeProgramComponent.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 import useUserHttp from '@hooks/queries/useUserQuery';
 import { ListInterface } from '@interfaces/listInterface';
 import { UserInterface } from '@interfaces/userInterface';
 
-
 const GoodByeComponent = () => {
+  const navigate = useNavigate();
+
+  const moveProgramList = () => {
+    navigate('/program', {state: {pgType: 'goodBye'}});
+  };
 
   return (
     <React.Fragment>
-        <div className='good' id='goodBye'>
+        <div className='good' id='goodBye' onClick={moveProgramList}>
             <p>굿바이 피로</p>
             <span>숲 활동, 운동, 명상 프로그램이 각 10-30분 정도로 총 4~6가지의 활동으로 운영.</span>
         </div>
--- src/components/program/GoodSleepProgramComponent.tsx
+++ src/components/program/GoodSleepProgramComponent.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 import useUserHttp from '@hooks/queries/useUserQuery';
 import { ListInterface } from '@interfaces/listInterface';
 import { UserInterface } from '@interfaces/userInterface';
 
-
 const GoodSleepComponent = () => {
+  const navigate = useNavigate();
+
+  const moveProgramList = () => {
+    navigate('/program', {state: {pgType: 'goodNight'}});
+  };
 
   return (
     <React.Fragment>
-        <div className='good' id='goodSleep'>
+        <div className='good' id='goodSleep' onClick={moveProgramList}>
             <p>웰컴 굿잠</p>
             <span>숲 활동, 운동, 명상 프로그램이 각 10-30분 정도로 총 4~6가지의 활동으로 운영.</span>
         </div>
