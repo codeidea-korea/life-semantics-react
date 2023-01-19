31be5689609e1a0a7bc3238938060fc2e0f5a1d4
Simin-Kim <show3391@gmail.com>
2023-01-19 19:53:16 +0900

    일일설문내역 api 연동

--- src/pages/survey/SurveyList.tsx
+++ src/pages/survey/SurveyList.tsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import TitleHeadComponent from "@/components/head/TitleHeadComponent";
+import useAxios from "@/hooks/useAxios";
+import { useRecoilValue } from "recoil";
+import { userState } from "@/states/userState";
 
 const SurveyList = () => {
-    const weekHandle = () => {};
+    const user = useRecoilValue(userState);
+    const api = useAxios();
+    const [week, setWeek] = useState(2);
+    const radioRef = useRef<HTMLInputElement>(null);
+    
+    useEffect(() => {
+        radioRef.current?.click();
+    }, [])
+
+    useEffect(() => {
+        getDailyHistory(week);
+    }, [week]);
+
+    const getDailyHistory = async (week: number) => {
+        await api
+            .get(`/usr/surveys/daily-history?week=${week}`, {headers: {Authorization: `Bearer ${user.accessToken}`}})
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    const weekHandle = (event: React.MouseEvent<HTMLInputElement>) => {
+        const {name, value} = event.currentTarget;
+        console.log(name, value)
+    };
+    
     return (
         <React.Fragment>
             <TitleHeadComponent name="일일 설문 내역" targetUrl="" />
@@ -13,15 +44,15 @@ const SurveyList = () => {
                 </div>
                 <div className="weekBtn">
                     <span>
-                        <input type="radio" id="week2" name="week"></input>
+                        <input type="radio" id="week2" name="week" value='2' onClick={() => setWeek(2)} ref={radioRef}></input>
                         <label htmlFor="week2">2주</label>
                     </span>
                     <span>
-                        <input type="radio" id="week4" name="week"></input>
+                        <input type="radio" id="week4" name="week" value={week} onClick={() => setWeek(4)}></input>
                         <label htmlFor="week4">4주</label>
                     </span>
                     <span>
-                        <input type="radio" id="week8" name="week"></input>
+                        <input type="radio" id="week8" name="week" value={week} onClick={() => setWeek(8)}></input>
                         <label htmlFor="week8">8주</label>
                     </span>
                 </div>
